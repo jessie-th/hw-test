12ec19cee2a9f53da8065a929d84af4278738a18
jessie-th <43228211+jessie-th@users.noreply.github.com>
2020-09-25 09:21:29 +0800

    Add files via upload

---
+++ p1.test_average.py
@@ -0,0 +1,26 @@
+# This program gets three test scores and displays
+# their average.  It congratulates the user if the
+# average is a high score.
+
+# Global constant for a high score
+HIGH_SCORE = 95
+
+# Get the three test scores.
+test1 = int(input('Enter the score for test 1: '))
+test2 = int(input('Enter the score for test 2: '))
+test3 = int(input('Enter the score for test 3: '))
+
+# Calculate the average test score.
+average = _________???________
+
+# Print the average.
+print('The average score is', average)
+
+# If the average is a high score,
+# congratulate the user.
+if _______???______
+    print('Congratulations!')
+    print('That is a great average!')
+    
+
+
